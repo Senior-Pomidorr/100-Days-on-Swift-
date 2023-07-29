ea2f81b7e85dd4aec9a1c5e8f68aa3a10ba5bc51
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-07-30 01:11:02 +0300

    finish the Word Scramble

--- Checker/Lession 4/WordScramble.swift
+++ Checker/Lession 4/WordScramble.swift
@@ -11,16 +11,29 @@ struct WordScramble: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var score = 0
     
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
     var body: some View {
         NavigationStack {
+            
             List {
                 Section {
                     TextField("Enter your word", text: $newWord)
                         .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    VStack() {
+                        Text("Your score is - \(score)")
+                    }
+                    .padding(50)
+                    .frame(height: 60)
+                    .font(.system(size: 28))
+                    .font(.headline)
+                    .foregroundColor(.mint)
                 }
                 
                 Section {
@@ -42,13 +55,24 @@ struct WordScramble: View {
             } message: {
                 Text(errorMessage)
             }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        startGame()
+                    }) {
+                        Text("Restart")
+                    }
+                }
+            }
+            
         }
+        
     }
     
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard answer.count > 0 else { return }
+        guard answer.count > 3 else { return }
         
         guard isOriginal(word: answer) else {
             wordError(title: "Word used alredy", message: "Ne more original")
@@ -66,6 +90,7 @@ struct WordScramble: View {
         
         withAnimation {
             usedWords.insert(answer, at: 0)
+            score += 1
         }
         newWord = ""
     }
@@ -75,6 +100,7 @@ struct WordScramble: View {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "Default word"
+                score = 0
                 return
             }
         }
