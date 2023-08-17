66f4f26fb900da809a3dbe6c67041cca449c1ed8
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-08-18 00:26:27 +0300

    finish drawing challenge

--- Checker/Lession 7/Drawing.swift
+++ Checker/Lession 7/Drawing.swift
@@ -12,43 +12,73 @@ import SwiftUI
 struct Drawing: View {
     @State private var lineWidth: CGFloat = 1.0
     @State private var shadow = 1.0
+    @State private var colorCycle = 0.0
+    
+    var amount = 0.0
+    var steps = 100
     
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            path.move(to: CGPoint(x: 160, y: 300))
-            path.addLine(to: CGPoint(x: 160, y: 600))
-            path.addLine(to: CGPoint(x: 240, y: 600))
-            path.addLine(to: CGPoint(x: 240, y: 300))
-            path.closeSubpath()
-        }
-        .fill(.blue)
-        .overlay(
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.2, green: 0.1, blue: 0.8), location: 0.3),
+                .init(color: Color(red: 1.0, green: 0.3, blue: 1.0), location: 0.4)
+            ], center: .top, startRadius: 450, endRadius: 420)
+            .ignoresSafeArea()
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
+            }
+            .padding(.bottom, 140)
+
             Path { path in
                 path.move(to: CGPoint(x: 200, y: 100))
                 path.addLine(to: CGPoint(x: 100, y: 300))
-                path.addLine(to: CGPoint(x: 160, y: 300))
-                path.addLine(to: CGPoint(x: 160, y: 300))
+                path.addLine(to: CGPoint(x: 300, y: 300))
+                path.addLine(to: CGPoint(x: 200, y: 100))
+                path.move(to: CGPoint(x: 160, y: 300))
                 path.addLine(to: CGPoint(x: 160, y: 600))
                 path.addLine(to: CGPoint(x: 240, y: 600))
                 path.addLine(to: CGPoint(x: 240, y: 300))
-                path.addLine(to: CGPoint(x: 300, y: 300))
-                path.addLine(to: CGPoint(x: 200, y: 100))
                 path.closeSubpath()
             }
-                .stroke(Color.pink, lineWidth: lineWidth)
-                .shadow(radius: shadow)
-        )
-        .onTapGesture {
-            withAnimation {
-                lineWidth = (lineWidth == 1.0) ? 8.0 : 1.0
+            .offset(x: 5, y: 100)
+            .fill(.blue)
+            .overlay(
+                Path { path in
+                    path.move(to: CGPoint(x: 200, y: 100))
+                    path.addLine(to: CGPoint(x: 100, y: 300))
+                    path.addLine(to: CGPoint(x: 160, y: 300))
+                    path.addLine(to: CGPoint(x: 160, y: 300))
+                    path.addLine(to: CGPoint(x: 160, y: 600))
+                    path.addLine(to: CGPoint(x: 240, y: 600))
+                    path.addLine(to: CGPoint(x: 240, y: 300))
+                    path.addLine(to: CGPoint(x: 300, y: 300))
+                    path.addLine(to: CGPoint(x: 200, y: 100))
+                    path.closeSubpath()
+                }
+                    .offset(x: 5, y: 100)
+                    .stroke(Color.pink, lineWidth: lineWidth)
+                    .shadow(radius: shadow)
+            )
+            .onTapGesture {
+                withAnimation {
+                    lineWidth = (lineWidth == 1.0) ? 8.0 : 1.0
                     shadow = (shadow == 1.0) ? 10.0 : 1.0
+                }
             }
         }
     }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+           var targetHue = Double(value) / Double(steps) + amount
+
+           if targetHue > 1 {
+               targetHue -= 1
+           }
+
+           return Color(hue: targetHue, saturation: 1, brightness: brightness)
+       }
 }
 
 
