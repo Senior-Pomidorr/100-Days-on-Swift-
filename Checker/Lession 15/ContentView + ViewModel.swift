ae41e84a42bf66475c2c1988484dd362f502fe0f
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-10-29 18:19:33 +0300

    add FaceID

--- Checker/Lession 15/ContentView + ViewModel.swift
+++ Checker/Lession 15/ContentView + ViewModel.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 import MapKit
+import LocalAuthentication
 
 extension LessionFifteen {
     @MainActor class ViewModel: ObservableObject {
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
-        @Published private(set) var locations = [Location]()
+        @Published private(set) var locations: [Location]
         @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
+        
         let savePath = FileManager.documentsDirectory.appendingPathComponent("SavePlaces")
         
         init() {
@@ -24,9 +27,19 @@ extension LessionFifteen {
             }
         }
         
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+        
         func addLocation() {
             let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
             locations.append(newLocation)
+            save()
         }
         
         func upadte(_ location: Location) {
@@ -35,6 +48,26 @@ extension LessionFifteen {
             
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
+                save()
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock the places"
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
+                    if success {
+                        Task { @MainActor in
+                                self.isUnlocked = true
+                            }
+                    } else {
+                        // error
+                    }
+                }
             }
         }
     }
